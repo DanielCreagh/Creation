42d950964b6c9257d365dcb3d4787b74f34d2169
Daniel Creagh <danielcreagh@Daniels-MacBook-Air.local>
2015-07-09 22:06:25 +0100

    using ng-repeat to angularize the site a bit more

--- www/angular/scripts/creaghApp.js
+++ www/angular/scripts/creaghApp.js
@@ -1,8 +1,14 @@
 angular.module('creationApp', [])
-.controller('creaghController', function($scope) {
+.controller('creaghController', function($scope, $http) {
 		var vm = this;
-		vm.nav = "About";
+		vm.nav = "Projects";
 		vm.projectsNav = "Main";
 
-		vm.style = {color:'red'};
+		$http.get('data/projectCategories.JSON').
+    success(function(data, status, headers, config) {
+      vm.projectcategories = data;
+    }).
+    error(function(data, status, headers, config) {
+      // log error
+    });
 	});
