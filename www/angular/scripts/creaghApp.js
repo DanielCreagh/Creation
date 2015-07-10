d8cf55d8f871409d2572f517ff13632655981c9b
Daniel Creagh <daniel@creagh.com>
2015-07-10 18:31:02 +0100

    ading data for contacts

--- www/angular/scripts/creaghApp.js
+++ www/angular/scripts/creaghApp.js
@@ -1,14 +1,54 @@
 angular.module('creationApp', [])
 .controller('creaghController', function($scope, $http) {
 		var vm = this;
-		vm.nav = "Projects";
+		vm.nav = "About";
 		vm.projectsNav = "Main";
 
 		$http.get('data/projectCategories.JSON').
-    success(function(data, status, headers, config) {
-      vm.projectcategories = data;
-    }).
-    error(function(data, status, headers, config) {
-      // log error
-    });
+      success(function(data, status, headers, config) {
+        vm.projectcategories = data;
+      }).
+      error(function(data, status, headers, config) {
+        console.log(data);
+      });
+
+    $http.get('data/contactsData.JSON').
+      success(function(data, status, headers, config) {
+        vm.contactsData = data;
+      }).
+      error(function(data, status, headers, config) {
+        console.log(data);
+      });
+
+    $http.get('data/flashProjectsData.JSON').
+      success(function(data, status, headers, config) {
+        vm.flashProjectsData = data;
+      }).
+      error(function(data, status, headers, config) {
+        console.log(data);
+      });
+
+    $http.get('data/html5ProjectsData.JSON').
+      success(function(data, status, headers, config) {
+        vm.html5ProjectsData = data;
+      }).
+      error(function(data, status, headers, config) {
+        console.log(data);
+      });
+
+    $http.get('data/iOSProjectsData.JSON').
+      success(function(data, status, headers, config) {
+        vm.iOSProjectsData = data;
+      }).
+      error(function(data, status, headers, config) {
+        console.log(data);
+      });
+
+    $http.get('data/otherProjectsData.JSON').
+      success(function(data, status, headers, config) {
+        vm.otherProjectsData = data;
+      }).
+      error(function(data, status, headers, config) {
+        console.log(data);
+      });
 	});
